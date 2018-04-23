8a4f6776a75b645d58b070bdf5fbf4875bc83a7f
paridhi jain <paridhijain@paridhis-MacBook-Air.local>
2018-04-22 20:10:54 -0400

    added utility method - namedtuple_to_dict

---
+++ src/util.py
@@ -0,0 +1,7 @@
+def namedtuple_to_dict(data):
+    """
+    Converts a namedtuple to a dictionary with the top-level key being the class name.
+    """
+    return  {type(data).__name__.lower(): dict(data.__dict__)}
+
+
