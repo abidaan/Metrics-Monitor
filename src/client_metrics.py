ae37c49ba014fe328adc2f2dd8a1b0a75d73523e
paridhi jain <paridhijain@paridhis-MacBook-Air.local>
2018-04-23 21:52:47 -0400

    added metrics.py to get sys metrics and client_metrics.py to create json dict of the sys metrics

---
+++ src/client_metrics.py
@@ -0,0 +1,17 @@
+import json
+
+import metrics
+import util
+
+
+metrics_dict = {} 
+
+metrics = metrics.get_metrics()
+
+for metric in metrics:
+	metrics_dict.update(util.namedtuple_to_dict(metric))
+
+
+json_metrics = json.dumps(metrics_dict)	
+
+print(json_metrics)
---
+++ src/metrics.py
@@ -0,0 +1,27 @@
+import psutil
+
+def get_metrics():
+	"""
+	Collects various system metrics.
+	:
+	"""
+	# Memory statistics
+
+	virtual_memory = psutil.virtual_memory()
+	swap_memory = psutil.swap_memory()
+
+	# CPU statistics
+
+	interrupts = psutil.cpu_stats()
+	cpu_frequency = psutil.cpu_freq()
+	cpu_time = psutil.cpu_times()
+
+	# Disk statistics
+
+	disk_usage = psutil.disk_usage('/')
+	disk_counters = psutil.disk_io_counters()
+
+	return [virtual_memory, swap_memory, interrupts, cpu_frequency, cpu_time, 
+			disk_usage, disk_counters]
+
+
