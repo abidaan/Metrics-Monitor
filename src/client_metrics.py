8be89ec07bdf9143d0b115bec9bb8f873b1b66b2
paridhi jain <paridhijain@paridhis-MacBook-Air.local>
2018-04-24 19:30:01 -0400

    edited the metrics file

--- src/client_metrics.py
+++ src/client_metrics.py
@@ -1,17 +1,13 @@
 import json
 
-import metrics
-import util
-
+from src.metric import metrics
+from src.metric import util
 
 metrics_dict = {}
-
 metrics = metrics.get_metrics()
 
 for metric in metrics:
     metrics_dict.update(util.namedtuple_to_dict(metric))
 
-
 json_metrics = json.dumps(metrics_dict)
 
-print(json_metrics)
--- src/metrics.py
+++ src/metrics.py
@@ -1,25 +1,64 @@
 import psutil
 
+import util
+
+metrics_dict = {"cpu": {}, "memory": {}, "disk": {}}
+
+
+def get_memory_metrics():
+    """
+    Collects memory metrics and returns a list of objects.
+    """
+    metrics = []
+    memory_dict = {}
+    metrics.append(psutil.virtual_memory())
+    metrics.append(psutil.swap_memory())
+
+    for metric in metrics:
+        memory_dict.update(util.namedtuple_to_dict(metric))
+
+    metrics_dict["memory"] = memory_dict
+
+
+def get_cpu_metrics():
+    """
+    Collects CPU metrics and returns a list of objects.
+    """
+    metrics = []
+    cpu_dict = {}
+    metrics.append(psutil.cpu_stats())
+    metrics.append(psutil.cpu_freq())
+    metrics.append(psutil.cpu_times())
+
+    for metric in metrics:
+        cpu_dict.update(util.namedtuple_to_dict(metric))
+
+    metrics_dict["cpu"] = cpu_dict
+
+
+def get_disk_metrics():
+    """
+    Collects disk metrics and returns a list of objects.
+    """
+    metrics = []
+    disk_dict = {}
+    metrics.append(psutil.disk_usage('/'))
+    metrics.append(psutil.disk_io_counters())
+
+    for metric in metrics:
+        disk_dict.update(util.namedtuple_to_dict(metric))
+
+    metrics_dict["disk"] = disk_dict
+
 
 def get_metrics():
     """
     Collects various system metrics and returns a list of objects.
     """
-    # Memory statistics
+    get_memory_metrics()
+    get_cpu_metrics()
+    get_disk_metrics()
 
-    virtual_memory = psutil.virtual_memory()
-    swap_memory = psutil.swap_memory()
 
-    # CPU statistics
-
-    interrupts = psutil.cpu_stats()
-    cpu_frequency = psutil.cpu_freq()
-    cpu_time = psutil.cpu_times()
-
-    # Disk statistics
-
-    disk_usage = psutil.disk_usage('/')
-    disk_counters = psutil.disk_io_counters()
-
-    return [virtual_memory, swap_memory, interrupts, cpu_frequency, cpu_time,
-            disk_usage, disk_counters]
+get_metrics()
+print(metrics_dict)
