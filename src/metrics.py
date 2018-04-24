f5f5c5e7f7d6514e8f05391283db52500e200d86
paridhi jain <paridhijain@paridhis-MacBook-Air.local>
2018-04-24 19:37:18 -0400

    small change

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,6 @@
 import psutil
 
-import util
+import src.util
 
 metrics_dict = {"cpu": {}, "memory": {}, "disk": {}}
 
@@ -9,12 +9,12 @@ def get_memory_metrics():
     """
     Collects memory metrics and returns a list of objects.
     """
-    metrics = []
+    memory_metrics = []
     memory_dict = {}
     metrics.append(psutil.virtual_memory())
     metrics.append(psutil.swap_memory())
 
-    for metric in metrics:
+    for metric in memory_metrics:
         memory_dict.update(util.namedtuple_to_dict(metric))
 
     metrics_dict["memory"] = memory_dict
@@ -24,13 +24,13 @@ def get_cpu_metrics():
     """
     Collects CPU metrics and returns a list of objects.
     """
-    metrics = []
+    cpu_metrics = []
     cpu_dict = {}
     metrics.append(psutil.cpu_stats())
     metrics.append(psutil.cpu_freq())
     metrics.append(psutil.cpu_times())
 
-    for metric in metrics:
+    for metric in cpu_metrics:
         cpu_dict.update(util.namedtuple_to_dict(metric))
 
     metrics_dict["cpu"] = cpu_dict
@@ -40,12 +40,12 @@ def get_disk_metrics():
     """
     Collects disk metrics and returns a list of objects.
     """
-    metrics = []
+    disk_metrics = []
     disk_dict = {}
     metrics.append(psutil.disk_usage('/'))
     metrics.append(psutil.disk_io_counters())
 
-    for metric in metrics:
+    for metric in disk_metrics:
         disk_dict.update(util.namedtuple_to_dict(metric))
 
     metrics_dict["disk"] = disk_dict
@@ -59,6 +59,3 @@ def get_metrics():
     get_cpu_metrics()
     get_disk_metrics()
 
-
-get_metrics()
-print(metrics_dict)
