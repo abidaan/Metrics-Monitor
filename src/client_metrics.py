d074bcac95728b4865f6fd72a194dbb06a469fe1
paridhi jain <paridhijain@paridhis-MacBook-Air.local>
2018-04-23 22:04:36 -0400

    corrected the liniting error

--- src/client_metrics.py
+++ src/client_metrics.py
@@ -9,9 +9,9 @@ metrics_dict = {}
 metrics = metrics.get_metrics()
 
 for metric in metrics:
-	metrics_dict.update(util.namedtuple_to_dict(metric))
+    metrics_dict.update(util.namedtuple_to_dict(metric))
 
 
-json_metrics = json.dumps(metrics_dict)	
+json_metrics = json.dumps(metrics_dict) 
 
 print(json_metrics)
--- src/metrics.py
+++ src/metrics.py
@@ -1,27 +1,24 @@
 import psutil
 
 def get_metrics():
-	"""
-	Collects various system metrics.
-	:
-	"""
-	# Memory statistics
+    """
+    Collects various system metrics and returns a list of objects.
+    """
+    # Memory statistics
 
-	virtual_memory = psutil.virtual_memory()
-	swap_memory = psutil.swap_memory()
+    virtual_memory = psutil.virtual_memory()
+    swap_memory = psutil.swap_memory()
 
-	# CPU statistics
+    # CPU statistics
 
-	interrupts = psutil.cpu_stats()
-	cpu_frequency = psutil.cpu_freq()
-	cpu_time = psutil.cpu_times()
+    interrupts = psutil.cpu_stats()
+    cpu_frequency = psutil.cpu_freq()
+    cpu_time = psutil.cpu_times()
 
-	# Disk statistics
+    # Disk statistics
 
-	disk_usage = psutil.disk_usage('/')
-	disk_counters = psutil.disk_io_counters()
+    disk_usage = psutil.disk_usage('/')
+    disk_counters = psutil.disk_io_counters()
 
-	return [virtual_memory, swap_memory, interrupts, cpu_frequency, cpu_time, 
-			disk_usage, disk_counters]
-
-
+    return [virtual_memory, swap_memory, interrupts, cpu_frequency, cpu_time,
+            disk_usage, disk_counters]
