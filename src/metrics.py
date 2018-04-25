9e0a185739c01211114965819ebde9ab447cf983
paridhi jain <paridhijain@paridhis-MacBook-Air.local>
2018-04-24 20:08:06 -0400

    small change4

--- src/metrics.py
+++ src/metrics.py
@@ -2,59 +2,48 @@ import psutil
 
 from src.util import namedtuple_to_dict
 
-metrics_dict = {"cpu": {}, "memory": {}, "disk": {}}
-
 
 def get_memory_metrics():
     """
     Collects memory metrics and returns a list of objects.
     """
-    memory_metrics = []
-    memory_dict = {}
-    memory_metrics.append(psutil.virtual_memory())
-    memory_metrics.append(psutil.swap_memory())
+    memory_metrics = {}
+    memory_metrics.update(namedtuple_to_dict(psutil.virtual_memory()))
+    memory_metrics.update(namedtuple_to_dict(psutil.swap_memory()))
 
-    for metric in memory_metrics:
-        memory_dict.update(namedtuple_to_dict(metric))
-
-    metrics_dict["memory"] = memory_dict
+    return {'memory': memory_metrics}
 
 
 def get_cpu_metrics():
     """
     Collects CPU metrics and returns a list of objects.
     """
-    cpu_metrics = []
-    cpu_dict = {}
-    cpu_metrics.append(psutil.cpu_stats())
-    cpu_metrics.append(psutil.cpu_freq())
-    cpu_metrics.append(psutil.cpu_times())
+    cpu_metrics = {}
+    cpu_metrics.update(namedtuple_to_dict(psutil.cpu_stats()))
+    cpu_metrics.update(namedtuple_to_dict(psutil.cpu_freq()))
+    cpu_metrics.update(namedtuple_to_dict(psutil.cpu_times()))
 
-    for metric in cpu_metrics:
-        cpu_dict.update(namedtuple_to_dict(metric))
-
-    metrics_dict["cpu"] = cpu_dict
+    return {'cpu': cpu_metrics}
 
 
 def get_disk_metrics():
     """
     Collects disk metrics and returns a list of objects.
     """
-    disk_metrics = []
-    disk_dict = {}
-    disk_metrics.append(psutil.disk_usage('/'))
-    disk_metrics.append(psutil.disk_io_counters())
+    disk_metrics = {}
+    disk_metrics.update(namedtuple_to_dict(psutil.disk_usage('/')))
+    disk_metrics.update(namedtuple_to_dict(psutil.disk_io_counters()))
 
-    for metric in disk_metrics:
-        disk_dict.update(namedtuple_to_dict(metric))
-
-    metrics_dict["disk"] = disk_dict
+    return {'disk': disk_metrics}
 
 
 def get_metrics():
     """
     Collects various system metrics and returns a list of objects.
     """
-    get_memory_metrics()
-    get_cpu_metrics()
-    get_disk_metrics()
+    metrics = {}
+    metrics.update(get_memory_metrics())
+    metrics.update(get_cpu_metrics())
+    metrics.update(get_disk_metrics())
+
+    return metrics
