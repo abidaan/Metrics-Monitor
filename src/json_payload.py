a99cd4533d1fd63c532c3662f817739af72f78e5
pjain17 <pjain9@ncsu.edu>
2018-05-01 19:07:39 -0400

    added sys_info.py (#6)
    * added sys_info.py

---
+++ src/json_payload.py
@@ -0,0 +1,23 @@
+import json
+
+from src.metrics import get_metrics
+from src.sys_info import get_sys_info
+
+
+def create_identifier_payload(only_identifier=True):
+    """
+    Creates a json payload with client information
+    """
+    if only_identifier:
+        return json.dumps(get_sys_info())
+
+    return get_sys_info()
+
+
+def create_msg_payload():
+    """
+    Creates the complete message payload
+    """
+    payload = {}
+    payload.update(create_identifier_payload(only_identifier=False))
+    return json.dumps(payload.update(get_metrics))
