885a5ff0792de19267196ae4b06e1f2e82567517
paridhi jain <paridhijain@paridhis-MacBook-Air.local>
2018-05-01 19:02:15 -0400

    removed global variable

--- src/json_payload.py
+++ src/json_payload.py
@@ -3,22 +3,21 @@ import json
 from src.metrics import get_metrics
 from src.sys_info import get_sys_info
 
-payload = {}
 
-
-def create_identifier_payload(onlyIdentifier=True):
+def create_identifier_payload(only_identifier=True):
     """
     Creates a json payload with client information
     """
-    if onlyIdentifier:
-        return json.dumps(payload.update(get_sys_info()))
+    if only_identifier:
+        return json.dumps(get_sys_info())
 
-    payload.update(get_sys_info())
+    return get_sys_info()
 
 
 def create_msg_payload():
     """
     Creates the complete message payload
     """
-    create_identifier_payload(onlyIdentifier=False)
+    payload = {}
+    payload.update(create_identifier_payload(only_identifier=False))
     return json.dumps(payload.update(get_metrics))
